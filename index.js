8b633762062e709d9cbe72e9221c3fcbbf04167b
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2023-04-03 07:42:27 -0300

    Configuracao do emulador

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import { registerRootComponent } from 'expo';
 
+import '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
+
 import App from './App';
+
+
+// if (__DEV__) {
+// 	firestore().terminate().then(() => {
+// 		firestore().clearPersistence().then(() => {
+// 			firestore().useEmulator('localhost', 8080);
+// 		}).catch((error) => {
+// 			console.log('Clearing persistence failed: ', error);
+// 		})
+// 	}).catch((error) => {
+// 		console.log('Terminating failed: ', error);
+// 	})
+// }
+
+firestore();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
